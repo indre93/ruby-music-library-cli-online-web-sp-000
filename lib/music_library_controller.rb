3b6f067d3257e08f92cf2bbc9b08bf98ee9ec54a
Indre Bakeer <indre.bakeer@gmail.com>
2019-03-03 22:41:28 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -54,7 +54,7 @@ class MusicLibraryController
     counter = 0
     puts "Please enter the name of an artist:"
     artist = gets.chomp
-    if artist = Artist.find_by_name(artist)
+    if artist = list_artists.find_by_name(artist)
       puts "#{counter += 1}. #{artist.song.name} - #{artist.genre.name}"
     end
   end
